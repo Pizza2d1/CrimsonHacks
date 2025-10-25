98eb41f3e0308f8e86db15c9a15acb15cef98aa4
Pizza2d1 <yyou9481138@gmail.com>
2025-10-24 22:08:47 -0600

    f

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ include_once("./includes/main_page.php");
   <main id="main" class="hero">
     <div class="hero-media">
       <div class="video-frame">
-        <video playsinline autoplay preload="metadata">
+        <video playsinline preload="metadata" autoplay>
           <source src="img/MartianAddress.mp4" type="video/mp4">
           Your browser does not support the video tag.
         </video>
