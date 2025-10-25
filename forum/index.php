070f7035229a26260f9502a2a93d025f2d92be47
Ethanscharlie <ethanscharlie@protonmail.com>
2025-10-24 22:15:38 -0600

    FIoew

--- forum/index.php
+++ forum/index.php
@@ -13,7 +13,7 @@ include_once("./includes/content.php");
       echo alien_ad(); # To left
     ?>
 
-      <div class='advertisement'><img src='/CrimsonHacks/img/img.png'</div>
+      <div class='advertisement'><img src='https://cdn.discordapp.com/attachments/1430037351475707986/1431491953668915329/image.png?ex=68fd9c45&is=68fc4ac5&hm=e9ef420044db8ca4f8a0d11e824dbb1c652d767aa9f259f3e96ef32243a7cdea&'</div>
     </body>
 </html>
 
