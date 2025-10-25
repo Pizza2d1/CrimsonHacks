8be069ea13489228cdf9530a9bd83d866b575c19
Pizza2d1 <yyou9481138@gmail.com>
2025-10-24 21:27:50 -0600

    penis

--- news/index.php
+++ news/index.php
@@ -35,11 +35,14 @@ Cydonia stands ready. No chili dog will be left unguarded. The city vows to prot
             <div class="news-content">
                 <h2><?= $newsTitle ?></h2>
                 <p><?= $newsContent ?></p>
+                <br><br>
+                <h2><?= $newsTitle2 ?></h2>
+                <p><?= $newsContent2 ?></p>
             </div>
 
             <!-- Right Image -->
             <div class="text-container right">
-                <h3><a href="This is text on the side"</h3>
+                <h3><a href="This is text on the side"</a></h3>
             </div>
         </div>
     </main>
