cee76b9eca8f758af974c19e35f8e24ba4c621d0
Pizza2d1 <yyou9481138@gmail.com>
2025-10-24 22:24:59 -0600

    1

--- forum/includes/content.php
+++ forum/includes/content.php
@@ -102,9 +102,8 @@ return "
 </div>
 ";}
 function alien_ad_left() {
-  echo "<div class='news-container'><div class='image-container left'>
-                <img src='https://cdn.discordapp.com/attachments/1430037351475707986/1431491953668915329/image.png?ex=68fd9c45&is=68fc4ac5&hm=e9ef420044db8ca4f8a0d11e824dbb1c652d767aa9f259f3e96ef32243a7cdea&' alt='Image 1' class='news-image'>
-        </div></div>":
+  echo "<div class='advertisement'><img src='/CrimsonHacks/img/ads/hot_aliens.png'</div>";
+
 }
 ?>
 
